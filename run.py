65281dba6afd792ed537e42637730be033d2e023
hhy <552483776@qq.com>
2017-10-25 23:38:19 +0800

    finish the simple reaction after login

--- run.py
+++ run.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/', methods=['POST', 'GET'])
-def index(name=None):
-    print("asdfasdfa")
-    if(request.method == "POST"):
-        print("get a post")
-    if(request.method == "GET"):
-        print("get a GET")
-    return app.send_static_file('login.html')
+# def index(name=None):
+#     print("asdfasdfa")
+#     if(request.method == "POST"):
+#         print("get a post")
+#         print(request.form["Username"])
+#         print(request.form["Password"])
+#         return "hello"
+#     if(request.method == "GET"):
+#         print("get a GET")
+#     return app.send_static_file('login.html')
 
 @app.route('/calculator.html', methods=['POST'])
 def login():
