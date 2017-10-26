d21eeb1efe895e4d2cf3182f9ef119ec8b9863a9
hhy <552483776@qq.com>
2017-10-26 14:00:07 +0800

    change the method to submit in login.html and copy calculator page to three

--- run.py
+++ run.py
@@ -7,22 +7,33 @@ from flask import request
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index(name=None):
-#     print("asdfasdfa")
-#     if(request.method == "POST"):
-#         print("get a post")
-#         print(request.form["Username"])
-#         print(request.form["Password"])
-#         return "hello"
-#     if(request.method == "GET"):
-#         print("get a GET")
+    # print("asdfasdfa")
+    # if(request.method == "POST"):
+    #     print("get a post")
+    #     print(request.form["Username"])
+    #     print(request.form["Password"])
+    #     return "hello"
+    # if(request.method == "GET"):
+    #     print("get a GET")
     return app.send_static_file('login.html')
 
-@app.route('/calculator.html', methods=['POST'])
+@app.route('/calculator_params.html', methods=['POST'])
 def login():
     print("login")
-    return app.send_static_file('calculator.html')
+    print(request.form["Username"])
+    print(request.form["Password"])
+
+    return app.send_static_file('calculator_params.html')
+
+@app.route('/calculator_device.html')
+def device():
+    return app.send_static_file('calculator_device.html')
+
+@app.route('/calculator_results.html')
+def results():
+    return app.send_static_file('alculator_results.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
